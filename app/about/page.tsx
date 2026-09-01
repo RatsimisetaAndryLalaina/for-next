c7643e6700a10f41c39b952b516d33a985210b31
RATSIMISETA Andry Lalaina <ratsimisetata@gmail.com>
2026-09-01 19:56:00 +0200

    Initial commit from Create Next App

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        ABOUT PAGE - <Link href="/">Go to homepage</Link>
+      </main>
+    </div>
+  );
+};
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Home() {
+  console.log('testing here');
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        HELLO WORLD
+        <section>
+        <Link href="/about">About</Link>
+      </section>
+      </main>
+
+      
+    </div>
+  );
+}
