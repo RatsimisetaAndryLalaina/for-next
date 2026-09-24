df70aa6512d4adea6e0f6cb5b016cb93c5aaf2c3
RATSIMISETA Andry Lalaina <ratsimisetata@gmail.com>
2026-09-24 08:55:53 +0200

    Use client

---
+++ app/community/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Link from "next/link";
+
+export default function CommunityPage() {
+    console.log('CommunityPage rendered');
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+            <h1>Community Page</h1>
+            <p>This is the community page.</p>
+        </main>
+
+        <Link href="/">Back to Home</Link>
+    </div>
+  );
+}
---
+++ app/request/[id]/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const {id} = await params;
+    console.log('idValue:', id);
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+            <h1>Dynamic Page</h1>
+            <p>Dynamic ID: {id}</p>
+            <section>
+                <Link href="/">Home</Link>
+            </section>
+        </main>
+    </div>
+  );
+};
